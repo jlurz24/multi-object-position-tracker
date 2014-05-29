5fa970999c5920c0ffcdfa2dabd073ef7e332f8d
jlurz24 <jlurz24@gmail.com>
2014-05-28 22:37:25 -0400

    Upgrade to Hydro

--- src/multi_object_detector.cpp
+++ src/multi_object_detector.cpp
@@ -11,6 +11,7 @@
 #include <message_filters/synchronizer.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/features/feature.h>
+#include <pcl/common/centroid.h>
 #include <cmath>
 #include <boost/math/constants/constants.hpp>
 #include <pcl/segmentation/extract_clusters.h>
@@ -19,7 +20,7 @@
 #include <pcl/common/geometry.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <image_geometry/pinhole_camera_model.h>
-#include <tf2/LinearMath/btVector3.h>
+#include <tf2/LinearMath/Vector3.h>
 #include <geometry_msgs/Vector3Stamped.h>
 #include <limits>
 #include <google/profiler.h>
@@ -178,22 +179,22 @@ private:
         ROS_DEBUG("Registration for blob events complete.");
     }
 
-    btVector3 intersection(const btVector3& n, const btVector3& p0, const btVector3& l,
-            const btVector3& l0) {
+    tf2::Vector3 intersection(const tf2::Vector3& n, const tf2::Vector3& p0, const tf2::Vector3& l,
+            const tf2::Vector3& l0) {
         // Make sure everything is normalized
         assert(n.length() - 1 < 1e-6);
         assert(l.length() - 1 < 1e-6);
 
-        btScalar denom = n.dot(l);
+        tfScalar denom = n.dot(l);
         if (fabs(denom) < 1e-6) {
-            return btVector3(numeric_limits<double>::quiet_NaN(),
+            return tf2::Vector3(numeric_limits<double>::quiet_NaN(),
                     numeric_limits<double>::quiet_NaN(), numeric_limits<double>::quiet_NaN());
         }
 
-        btScalar t = n.dot(p0 - l0) / denom;
+        tfScalar t = n.dot(p0 - l0) / denom;
         if (t < 0) {
             // Intersection behind the camera
-            return btVector3(numeric_limits<double>::quiet_NaN(),
+            return tf2::Vector3(numeric_limits<double>::quiet_NaN(),
                     numeric_limits<double>::quiet_NaN(), numeric_limits<double>::quiet_NaN());
         }
         // Now project the vector l to the plane.
@@ -317,7 +318,7 @@ private:
         for (unsigned int i = 0; i < blobClouds.size(); ++i) {
             Eigen::Vector4f centroid;
             ROS_DEBUG("Computing centroid for blob with %lu indices", blobClouds[i].indices.size());
-            compute3DCentroid(*allBlobs, blobClouds[i].indices, centroid);
+            pcl::compute3DCentroid(*allBlobs, blobClouds[i].indices, centroid);
 
             // Convert the centroid to a point stamped
             geometry_msgs::PointStamped resultPoint;
@@ -393,10 +394,11 @@ private:
         ROS_DEBUG("Blobs message has %lu blobs", blobsMsg->blobs.size());
         for (unsigned int k = 0; k < blobsMsg->blobs.size(); ++k) {
             const cmvision::Blob blob = blobsMsg->blobs[k];
-            if (objectName.size() > 0 && objectName != blob.colorName) {
-                ROS_DEBUG("Skipping blob named %s as it does not match", objectName.c_str());
-                continue;
-            }
+            // TODO: Add support to library
+            // if (objectName.size() > 0 && objectName != blob.colorName) {
+            //    ROS_DEBUG("Skipping blob named %s as it does not match", objectName.c_str());
+            //    continue;
+            //}
 
             ROS_DEBUG("Blob image dimensions. Left %u right %u top %u bottom %u width %u height %u",
                     blob.left, blob.right, blob.top, blob.bottom, blobsMsg->image_width,
